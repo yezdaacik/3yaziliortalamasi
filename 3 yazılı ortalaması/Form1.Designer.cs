ecc32bc4e5e2cc7e73e9c02d1d014b0099be6e15
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-02 12:34:43 +0300

    Add files via upload

---
+++ 3 yazılı ortalaması/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace _3_yazılı_ortalaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtYazili1 = new System.Windows.Forms.TextBox();
+            this.txtYazili2 = new System.Windows.Forms.TextBox();
+            this.txtYazili3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yazılı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yazılı 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yazılı 3";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(20, 140);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(81, 13);
+            this.lblOrtalama.TabIndex = 3;
+            this.lblOrtalama.Text = "Yazılı ortalaması";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(23, 177);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtYazili1
+            // 
+            this.txtYazili1.Location = new System.Drawing.Point(80, 19);
+            this.txtYazili1.Name = "txtYazili1";
+            this.txtYazili1.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili1.TabIndex = 5;
+            // 
+            // txtYazili2
+            // 
+            this.txtYazili2.Location = new System.Drawing.Point(80, 57);
+            this.txtYazili2.Name = "txtYazili2";
+            this.txtYazili2.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili2.TabIndex = 6;
+            // 
+            // txtYazili3
+            // 
+            this.txtYazili3.Location = new System.Drawing.Point(80, 94);
+            this.txtYazili3.Name = "txtYazili3";
+            this.txtYazili3.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 236);
+            this.Controls.Add(this.txtYazili3);
+            this.Controls.Add(this.txtYazili2);
+            this.Controls.Add(this.txtYazili1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtYazili1;
+        private System.Windows.Forms.TextBox txtYazili2;
+        private System.Windows.Forms.TextBox txtYazili3;
+    }
+}
+
---
+++ 3 yazılı ortalaması/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_yazılı_ortalaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+           
+            int yazili1 = Convert.ToInt32(txtYazili1.Text);
+            int yazili2 = Convert.ToInt32(txtYazili2.Text);
+            int yazili3 = Convert.ToInt32(txtYazili3.Text);
+
+            int sonuc = (yazili1 + yazili2 + yazili3) / 3 ;
+
+            lblOrtalama.Text = sonuc.ToString();
+
+        }
+    }
+}
